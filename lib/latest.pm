aaa58cc62a5e8526d5cc7406a7e3ac253f7727f4
Andy Armstrong <andy@hexten.net>
2009-06-23 21:35:41 +0100

    Bump version to 0.03

--- lib/latest.pm
+++ lib/latest.pm
@@ -19,11 +19,11 @@ latest - Use the latest Perl features
 
 =head1 VERSION
 
-This document describes latest version 0.02
+This document describes latest version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
